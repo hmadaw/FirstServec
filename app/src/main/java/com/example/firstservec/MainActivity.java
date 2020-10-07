7f6dfbfbdc4f5ef2cea3126f95c942216ca3837e
zizo <hmadazizo350@gmail.com>
2020-10-07 14:27:37 +0200

    h

--- app/src/main/java/com/example/firstservec/MainActivity.java
+++ app/src/main/java/com/example/firstservec/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.firstservec;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,7 +23,11 @@ Button start , stop ;
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startService(new Intent(getBaseContext() , MyServes.class));
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    ContextCompat.startForegroundService(getApplicationContext(),new Intent(MainActivity.this , MyServes.class));
+                }else {
+                    startService(new Intent(MainActivity.this , MyServes.class));
+                }
             }
         });
 
--- app/src/main/java/com/example/firstservec/MyServes.java
+++ app/src/main/java/com/example/firstservec/MyServes.java
@@ -1,21 +1,54 @@
 package com.example.firstservec;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.IBinder;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 
 public class MyServes extends Service {
-MediaPlayer mediaPlayer;
+    ;
+    private static final String CHANNEL_ID = "channel_id_1";
+    MediaPlayer mediaPlayer;
 Thread sound ;
     @Override
     public void onCreate() {
         super.onCreate();
+        Intent notificationIntent =  new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                0, notificationIntent, 0);
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("music")
+                .setContentText(getResources().getResourceName(R.raw.abo))
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentIntent(pendingIntent)
+                .build();
+
+        createNotificationChannel();
+
+        startForeground(1,notification);
         Toast.makeText(getApplicationContext(), "on create" , Toast.LENGTH_LONG).show();
-        hashCode()
+
+        createNotificationChannel();
+    }
+    private void createNotificationChannel() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+
+            NotificationChannel serviceChannel = new NotificationChannel(
+                    CHANNEL_ID, "music channel",NotificationManager.IMPORTANCE_DEFAULT);
+            serviceChannel.setDescription("channel for music");
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(serviceChannel);
+
+        }
     }
 
     @Nullable
@@ -33,7 +66,8 @@ Thread sound ;
         sound = new Thread(){
             public void run(){
                 mediaPlayer.start();
-            }
+
+        }
         };
         sound.start();
 
